b875b85d0b5320d52c800940ae901de1c3228e55
Marko Dorfhuber <markodorfhuber@gmx.de>
2017-01-06 10:08:09 +0100

    Improvements and bugfix in TcpdumpHostCollector collector module

--- src/insalata/scanner/modules/TcpdumpHostCollector.py
+++ src/insalata/scanner/modules/TcpdumpHostCollector.py
@@ -2,6 +2,7 @@ from insalata.helper.SSHWrapper import SSHWrapper
 import re
 import subprocess
 from insalata.model.Layer3Address import Layer3Address
+from insalata.scanner.modules import base
 
 def scan(graph, connectionInfo, logger, thread):
     """
@@ -32,32 +33,34 @@ def scan(graph, connectionInfo, logger, thread):
     monServer = connectionInfo["monitoringServer"]
     logger.debug("Starting host scan using tcpdump on monitoring server {0}".format(monServer))
 
+    options = "-n -i any not host localhost and not host 127.0.0.1 and not arp and not rarp and not ip6" # By now we do not support IPv6
     if monServer != "localhost":
-        ssh = SSHWrapper()
-        ssh.connect(monServer) # TODO no connection possible
-        stdout = ssh.executeTcpdump("-i any host not localhost and not arp and not rarp")
+        ssh = base.getSSHConnection(monServer)
+        if ssh is None: #No ssh connecton is possible -> Skip this host
+            logger.error("No ssh connection to the monitoring server {} is possible in the tcpdump collector module!".format(monServer))
+            return
+        stdout = ssh.executeTcpdump(options)
     else:
-        proc = subprocess.Popen("tcpdump -i any host not localhost and not arp and not rarp and not ip6".split(" "), stdout=subprocess.PIPE)
+        proc = subprocess.Popen("tcpdump {}".format(options).split(" "), stdout=subprocess.PIPE)
         stdout = proc.stdout
 
     while(thread.stopRequested):
         packet = stdout.readline()
         if isinstance(packet, bytes):
             packet = packet.decode("ascii")
-        #logger.debug(packet)
+
         # Start ssh connection and read output line by line
         res = re.search(r"[0-9\:\.]+([^,]+,)? IP ([^ ]+) >", packet)
         if not res:
             continue
         res = res.group(2)
 
-        # We know that the src host exists in the network
-        src = ".".join(res.split(".")[:4])
-        #logger.debug("Found host in physical environment: {0}".format(src))
-        #logger.debug(packet)
+        src = ".".join(res.split(".")[:4]) # Source address of the packet
 
         location = graph.getOrCreateLocation("physical", name, timeout)
 
+        # We only want to add the address as a host to the graph if no l3address with the ID already exists
+        # This would lead to an incorrect graph state, as one host is represented multiple times
         if len([a for a in graph.getAllNeighbors(Layer3Address) if a.getID() == src]) == 0:
             host = graph.getOrCreateHost(src, name, timeout, location=location)
             host.setLocation(location)
